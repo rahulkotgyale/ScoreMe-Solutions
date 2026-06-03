32324248fc62deb4df22dc0787c5928c0e00c861
unknown <rrkotgyale18@gmail.com>
2026-06-03 17:42:17 +0530

    Initial commit

--- pipeline-scheduler/src/main/java/com/scoreme/pipeline_scheduler/exception/ExceptionHandle.java
+++ pipeline-scheduler/src/main/java/com/scoreme/pipeline_scheduler/exception/ExceptionHandle.java
@@ -8,8 +8,34 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ExceptionHandle {
 
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(
+            RuntimeException ex) {
 
+        return new ResponseEntity<>(
+                ex.getMessage(),
+                HttpStatus.BAD_REQUEST);
+    }
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(
+            IllegalArgumentException ex) {
+
+        return new ResponseEntity<>(
+                ex.getMessage(),
+                HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<String> handleNullPointerException(
+            NullPointerException ex) {
+
+        return new ResponseEntity<>(
+                "Null value encountered: " + ex.getMessage(),
+                HttpStatus.BAD_REQUEST);
+    }
+
+   
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(
             Exception ex) {
